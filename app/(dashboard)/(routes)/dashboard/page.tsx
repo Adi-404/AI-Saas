d57bd46cb5a0e2d605cb19696f4a44aba1d435e0
Aditya Mahapatra <adinm@Adityas-Laptop.local>
2024-07-16 02:11:33 +0530

    clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
-   <p>
+    <div>
+
+    <p>
    DashBoard Page (Protected)
+     
    </p>
+
+   <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
 
