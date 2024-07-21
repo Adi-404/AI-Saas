63b4af5d3d155b5b4cee099b944da5c63c5ed690
Aditya Mahapatra <adinmahapatra03@gmail.com>
2024-07-22 02:26:03 +0530

    side bar component

---
+++ components/sidebar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex felx-xol h-full
+        bg-[#111827] text-white">
+
+            Hello Sidebar Component 
+
+        </div>
+    );
+
+}
+
+export default Sidebar;
